6990bcb1188644c6774c3ff89d56e16c42a43d15
daniel.orozco <danor.dcor@gmail.com>
2021-04-19 11:41:57 -0500

    Redis

--- app/app.py
+++ app/app.py
@@ -1,9 +1,20 @@
+import socket
+
+import redis
 from flask import Flask
+from flask_restplus import Resource, Api
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Flask application running properly!'
+@api.route('/hello')
+class Hello(Resource):
+    def get(self):
+        return {'status': 'Flask application running properly!'}
 
+
+@api.route('/redis')
+class Redis(Resource):
+    def get(self):
+        r.incr('views')
+        return {'count': f"This site has been visited {int(r.get('views'))}, the hostname is {socket.gethostname()}"}
